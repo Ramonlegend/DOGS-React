ae0a9b112067274c298045c20e42c057549cceaf
Ramon.god <ramongodinho01@gmail.com>
2023-07-27 13:52:53 -0300

    validate token

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({
-  label,
-  type,
-  name,
-  value,
-  setValue,
-  onChange,
-  error,
-  onBlur,
-}) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -21,6 +12,7 @@ const Input = ({
         name={name}
         className={styles.input}
         type={type}
+        value={value}
         onChange={onChange}
         onBlur={onBlur}
       />
